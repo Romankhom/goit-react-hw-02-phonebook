d49cff43ac3a3dc1eb305b6f5538cf3f69463a21
Romankhom <homenko.r.v@gmail.com>
2020-08-29 20:13:56 +0300

    Updates

---
+++ precache-manifest.7f9c9cfc278e921a73412f843c2dee80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f95c764ca7e02b26e31304ab42eada2",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "fecbf46d61ff698d83dc",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.805241a2.chunk.css"
+  },
+  {
+    "revision": "35f8d88735ca37d07e76",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.33ef2167.chunk.js"
+  },
+  {
+    "revision": "fecbf46d61ff698d83dc",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.23010be9.chunk.js"
+  },
+  {
+    "revision": "6bc075c8c613983aef96",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3a27eb80.js"
+  }
+]);
